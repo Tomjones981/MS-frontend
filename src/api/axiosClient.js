81f6cd3d313798a299266f559b0617bb26e99926
Tomjones981 <127879265+Tomjones981@users.noreply.github.com>
2025-10-02 12:28:11 +0800

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'https://darkturquoise-worm-880823.hostingersite.com/api',
-  // baseURL: 'http://127.0.0.1:8000/api',
+  // baseURL: 'https://darkturquoise-worm-880823.hostingersite.com/api',
+  baseURL: 'http://127.0.0.1:8000/api',
   
 });
 
